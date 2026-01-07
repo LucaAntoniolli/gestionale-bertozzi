802f691aff27c633c70976b3931baa6256a03b1b
LucaAntoniolli <antoniolli@nemesitalia.it>
2026-01-07 16:06:08 +0100

    Script per caricamento clienti

---
+++ database/script-caricamento-clienti.sql
@@ -0,0 +1,101 @@
+INSERT INTO dbo.Cliente
+(
+    RagioneSociale,
+    CodiceInterno,
+    PartitaIva,
+    CodiceFiscale,
+    Indirizzo,
+    Comune,
+    CAP,
+    Provincia,
+    Nazione,
+    Telefono,
+    Email,
+    ModalitaPagamentoId,
+    Sdi,
+    Tipo
+)
+VALUES
+(N'ARCOS COSTRUZIONI S.R.L.', N'michelap', N'03659560985', N'03659560985', N'VIA BAGUTTA 13', N'Milano', N'20121', N'MI', N'Italia', NULL, NULL, NULL, N'KRRH6B9', N'Azienda'),
+
+(N'CAMOZZI GROUP SPA', NULL, N'03519280170', NULL, N'Via R. Rubattino 81', N'Milano', N'20134', N'MI', N'Italia', N'0221711751', N'info@camozzi.com', NULL, NULL, N'Azienda'),
+
+(N'CONAD CENTRO NORD SOCIETA'' COOPERATIVA', NULL, N'01751820356', N'01751820356', N'VIA JOHN FITZGERALD KENNEDY, 5', N'CAMPEGINE', N'42040', N'RE', N'Italia', NULL, NULL, NULL, N'SN4CSRI', N'Azienda'),
+
+(N'COPERTECH SRL', NULL, N'08693690961', N'08693690961', N'Via Durini 15', N'Milano', N'20122', N'MI', N'Italia', NULL, N'amministrazione@copertech.it', NULL, N'KRRH6B9', N'Azienda'),
+
+(N'Cospe Srl', NULL, N'03223690169', N'03223690169', N'Via Mortini 16', N'San Paolo d''Argon', N'24060', N'BG', N'Italia', NULL, N'amministrazione@cospe.eu', NULL, NULL, N'Azienda'),
+
+(N'Cospe Srl', N'002', N'03223690169', N'03223690169', N'Via Mortini 16', N'San Paolo d''Argon', N'24060', N'BG', N'Italia', NULL, N'amministrazione@cospe.eu', NULL, N'M5UXCR1', N'Azienda'),
+
+(N'Costa e C SRL', NULL, N'03856440981', N'03856440981', N'Via Giacomo Matteotti 14', N'Ghedi', N'25016', N'BS', N'Italia', NULL, NULL, NULL, NULL, N'Azienda'),
+
+(N'DOMUS ING & ARCH S.R.L.', NULL, N'03557580168', N'03557580168', N'VIA PASTRENGO 1/C', N'SERIATE', N'24068', N'BG', N'Italia', NULL, N'p.gatti@studiodomus.net', NULL, NULL, N'Professionista'),
+
+(N'EDIL FORNITURE SRL', NULL, N'00931340160', N'00931340160', N'via Isonzo 36', N'Romano di Lombardia', N'24058', N'BG', N'Italia', NULL, NULL, NULL, N'TPICRCA', N'Azienda'),
+
+(N'EDIL PADANA SOCIETA'' A RESPONSABILITA'' LIMITATA SEMPLIFICATA', NULL, N'04051250985', N'04051250985', N'VIA BELZAGNO 32', N'TRENZANO', N'25030', N'BS', N'Italia', NULL, NULL, NULL, N'SUBM70N', N'Azienda'),
+
+(N'EDIL PADANA SRLS', NULL, N'04051250985', N'04051250985', N'VIA BELZAGNO 32', N'Trenzano', N'25030', N'BS', N'Italia', NULL, NULL, NULL, NULL, N'Azienda'),
+
+(N'Engineering 2K S.p.A.', NULL, N'01297450338', N'01297450338', N'Strada 1 Palazzo E1', N'Assago Milanofiori', N'20057', N'MI', N'Italia', NULL, NULL, NULL, N'7HE8RN5', N'Professionista'),
+
+(N'Esselunga SpA', N'012', N'04916380159', N'01255720169', N'Via Giambologna', N'Limito', N'20096', N'MI', N'Italia', NULL, N'fattureservizi@esselunga.it', NULL, N'MZO2A0U', N'Azienda'),
+
+(N'FEDIL COSTRUZIONI SRL', NULL, N'00951800192', N'00951800192', N'VIA DEGLI ARTIGIANI 18', N'PANDINO', N'26025', N'CR', N'Italia', NULL, N'federica@fedilcostruzioni.it', NULL, N'SUBM70N', N'Azienda'),
+
+(N'Futura Costruzioni srl', N'009', N'IT03834220984', N'03834220984', N'Via Martiri della Libertà 281/A', N'Roncadelle', N'25030', N'BS', N'Italia', NULL, N'costruzioni.futurasrl@gmail.com', NULL, N'SUBM70N', N'Azienda'),
+
+(N'Futura Costruzioni srl', NULL, N'03834220984', N'03834220984', N'Via Martiri della Libertà 281/A', N'Roncadelle', N'25030', N'BS', N'Italia', NULL, N'costruzioni.futurasrl@gmail.com', NULL, N'SUBM70N', N'Azienda'),
+
+(N'GB&Partners S.r.l.', NULL, NULL, NULL, N'Via Varali 37', N'Codogno', N'26845', N'LO', N'Italia', NULL, NULL, NULL, NULL, N'Professionista'),
+
+(N'GB&PARTNERS Srl', NULL, N'05966150962', N'05966150962', N'Via Varalli 37', N'Codogno', N'26845', N'LO', N'Italia', NULL, N'raffaella.bianchi@gbepartners.it', NULL, N'SUBM70N', N'Professionista'),
+
+(N'GB&PARTNERS srl', N'001', N'05966150962', N'05966150962', N'Via Varalli 37', N'Codogno', N'26845', N'LO', N'Italia', NULL, N'raffaella.bianchi@gbepartners.it', NULL, N'SUBM70N', N'Professionista'),
+
+(N'Geom Paolo Zini', N'006', N'03391600172', N'ZNIPLA73D29F205R', N'Piazza Degli Alpini 1', N'Travagliato', N'25039', N'BS', N'Italia', NULL, N'info@szsrl.it', NULL, N'KRRH6B9', N'Professionista'),
+
+(N'Geom. Paolo Zini', NULL, N'03391600172', N'ZNIPLA73D29F205R', N'Piazza Degli Alpini 1', N'Travagliato', N'25039', N'BS', N'Italia', NULL, NULL, NULL, NULL, N'Professionista'),
+
+(N'Geom. Roberto Berardelli', NULL, N'03535050177', N'BRRRRT64M19F205V', N'Piazza degli Alpini, 2', N'Travagliato', N'25039', N'BS', N'Italia', NULL, NULL, NULL, NULL, N'Professionista'),
+
+(N'Geom. Santi Ivan', NULL, N'03344740174', N'SNTVNI73L02B157K', N'Piazza Degli Alpini 1', N'Travagliato', N'25039', N'BS', N'Italia', NULL, N'studiotecnicosz@virgilio.it', NULL, N'KRRH6B9', N'Professionista'),
+
+(N'Ing. Massetti Riccardo', NULL, N'03177660986', N'MSSRCR79H21C618N', N'Via Romanino 10', N'Pontevico', N'25026', N'BS', N'Italia', NULL, N'ing.riccardomassetti@gmail.com', NULL, NULL, N'Professionista'),
+
+(N'ISOCELL PRECOMPRESSI S.P.A.', NULL, N'03057160164', N'03057160164', N'STRADA PROVINCIALE 122 FRANCESCA', N'POGNANO', N'24040', N'BG', N'Italia', NULL, NULL, NULL, N'SUBM70N', N'Azienda'),
+
+(N'Kronos s.r.l.', NULL, N'03279040988', N'03279040988', N'Via dell''Artigianato 77', N'Travagliato', N'25039', N'BS', N'Italia', NULL, N'fatture@kronos-srl.eu', NULL, N'KRRH6B9', N'Azienda'),
+
+(N'LATT-DO Srl', NULL, N'03431900178', N'03431900178', N'Via Cavour 100', N'Botticino', N'25082', N'BS', N'Italia', NULL, N'info@lattdo.it', NULL, N'ACTKBBZ', N'Azienda'),
+
+(N'PGM srl', NULL, N'02947260168', N'02947260168', N'Via Alessandro Manzoni 26', N'San Paolo d''Argon', N'24060', N'BG', N'Italia', NULL, NULL, NULL, N'M5UXCR1', N'Azienda'),
+
+(N'Piovanelli Mario', NULL, N'02370320984', N'PVNMRA76M28B157T', N'Corso Silvio Bonomelli 41', N'Rovato', N'25038', N'BS', N'Italia', NULL, NULL, NULL, N'M5UXCR1', N'Professionista'),
+
+(N'Riviera Group srl', NULL, N'04159810169', N'04159810169', N'Via San Remigio 21', N'Endine Gaiano', N'24060', N'BG', N'Italia', NULL, NULL, NULL, N'SUBM70N', N'Azienda'),
+
+(N'SANDRINI METALLI S.P.A.', NULL, N'02902440169', N'02902440169', N'VIA P. TOGLIATTI, 18/A', N'COSTA VOLPINO', N'24062', N'BG', N'Italia', NULL, NULL, NULL, N'W7YVJK9', N'Azienda'),
+
+(N'STEN SRL', NULL, N'04668400981', N'04668400981', N'Piazza Alpini 2', N'Travagliato', N'25039', N'BS', N'Italia', NULL, NULL, NULL, NULL, N'Professionista'),
+
+(N'Structural Engineering di Alessandro Bertozzi', NULL, N'02869750980', N'BRTLSN80B08B157Y', N'Piazza Alpini 2', N'Travagliato', N'25039', N'BS', N'Italia', N'0303451896', N'alessandro@bertozzingegneria.it', NULL, NULL, N'Professionista'),
+
+(N'Studio Artecnica', NULL, N'01929630984', N'01929630984', N'Via Bettinzoli 2', N'Carpenedolo', N'25013', N'BS', N'Italia', NULL, NULL, NULL, NULL, N'Professionista'),
+
+(N'SZ Srl', NULL, N'02813480981', N'02813480981', N'Piazza Alpini 1', N'Travagliato', N'25039', N'BS', N'Italia', NULL, NULL, NULL, N'KRRH6B9', N'Professionista'),
+
+(N'Techbau spa', NULL, N'06336690968', N'06336690968', N'Piazza Giovine Italia 3', N'Milano', N'20123', N'MI', N'Italia', NULL, N'amministrazione@techbau.it', NULL, N'1GQBURM', N'Azienda'),
+
+(N'TECNOPROGETTI S.R.L.', NULL, N'02884810165', N'02884810165', N'VIA DIVISIONE ACQUI, 14/A', N'TELGATE', N'24060', N'BG', N'Italia', NULL, N'm.muratori@tecnoprogetti.org; n.zorba@tecnoprogetti.org', NULL, N'BA6ET11', N'Azienda'),
+
+(N'TECOMET S.R.L.', NULL, N'03544980406', N'03544980406', N'VIA ETTORE BENINI 4', N'FORLÌ', N'47121', N'FC', N'Italia', NULL, N'A.CALBI@TECOMET.IT', NULL, N'M5UXCR1', N'Azienda'),
+
+(N'TECOMET SRL', NULL, N'03544980406', N'03544980406', N'VIA ETTORE BENINI 4', N'FORLI', N'47121', N'FC', N'Italia', NULL, N'a.calbi@tecomet.it', NULL, N'M5UXCR1', N'Azienda'),
+
+(N'The Blossom Avenue Management srl', NULL, N'08728030969', N'08728030969', N'Corso Italia 13', N'Milano', N'20122', N'MI', N'Italia', NULL, NULL, NULL, N'SUBM70N', N'Professionista'),
+
+(N'The Blossom Avenue Partners', N'001', N'10465020963', N'10465020963', N'Corso Italia 13', N'Milano', N'20122', N'MI', N'Italia', NULL, N'marco.facchinetti@theblossomavenue.com', NULL, N'SUBM70N', N'Professionista'),
+
+(N'The Blossom Avenue Partners', NULL, N'10465020963', N'10465020963', N'Corso Italia 13', N'Milano', N'20122', N'MI', N'Italia', NULL, N'antonella.rigamonti@theblossomavenue.com', NULL, NULL, N'Professionista');
